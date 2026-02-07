c2d5da77ccbdabb6325d231b3a2f2a68875468a1
MGallo-Code <mgallo.code@gmail.com>
2026-02-07 00:46:29 -0500

    Update config.go to correctly fetch env variables, my first go code :)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,47 @@
 // Package config loads and validates environment variables.
-//
-// All configuration comes from environment variables (never hardcoded).
-// Required: DATABASE_URL, REDIS_URL
-// Optional: PORT, SESSION_TTL, COOKIE_DOMAIN, COOKIE_SECURE
 package config
+
+import (
+	"fmt"
+	"os"
+)
+
+// Define structure for env config variables
+type Config struct {
+	DatabaseURL  string
+	RedisURL     string
+	Port         string
+	CookieDomain string
+	CookieSecure bool
+}
+
+func LoadConfig() (*Config, error) {
+	// Create config obj
+	cfg := &Config{}
+
+	// Attempt to get db url, if missing, err
+	cfg.DatabaseURL = os.Getenv("DATABASE_URL")
+	if cfg.DatabaseURL == "" {
+		return nil, fmt.Errorf("DATABASE_URL is required")
+	}
+
+	// Attempt to get redis url, if missing, err
+	cfg.RedisURL = os.Getenv("REDIS_URL")
+	if cfg.RedisURL == "" {
+		return nil, fmt.Errorf("REDIS_URL is required")
+	}
+
+	// Attempt to get port num, default to 7865
+	cfg.Port = os.Getenv("PORT")
+	if cfg.Port == "" {
+		cfg.Port = "7865"
+	}
+
+	// Attempt to get cookie domain
+	cfg.CookieDomain = os.Getenv("COOKIE_DOMAIN")
+
+	// Attempt to get cookie secure value as bool
+	cfg.CookieSecure = os.Getenv("COOKIE_SECURE") != "false"
+
+	return cfg, nil
+}
